5069fe0a8df67dd42d743c18eaade05dcae9d3ba
BhargavPabbaraju <bhargav.n.shyam@gmail.com>
2025-03-16 22:52:10 -0700

    complete header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Hello, World!");
+import "./styles.css";
+import portraitImg from "../assets/portrait.jpg";
+import { renderSocials } from "./socials";
+
+function renderImage(id, image) {
+  const img = document.getElementById(id);
+  img.src = image;
+}
+
+function renderInitialContent() {
+  renderImage("portrait-img", portraitImg);
+  renderSocials();
+}
+
+renderInitialContent();
---
+++ src/socials.js
@@ -0,0 +1,40 @@
+const SOCIALS = {
+  GITHUB: {
+    icon: "devicon-github-original",
+    name: "Github",
+    link: "https://github.com/BhargavPabbaraju",
+  },
+  LINKEDIN: {
+    icon: "devicon-linkedin-plain",
+    name: "Linkedin",
+    link: "https://linkedin.com/in/BhargavPabbaraju",
+  },
+  ITCH: {
+    icon: "devicon-itch-io",
+    name: "Itch",
+    link: "https://bhargavpabbaraju.itch.io/",
+  },
+};
+
+function renderSocial(social) {
+  const i = document.createElement("i");
+  i.classList.add(social.icon);
+  i.classList.add("dev-icon");
+
+  const link = document.createElement("a");
+  link.href = social.link;
+
+  link.appendChild(i);
+
+  return link;
+}
+
+export function renderSocials() {
+  const divs = document.getElementsByClassName("socials");
+  Array.from(divs).forEach((div) => {
+    div.replaceChildren();
+    Object.values(SOCIALS).forEach((social) => {
+      div.appendChild(renderSocial(social));
+    });
+  });
+}
